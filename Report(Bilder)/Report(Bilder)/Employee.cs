d454848d073250147b14d2f4cd27fece89263cc7
Roman Pletiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2026-02-26 12:50:47 +0200

    Update Employee.cs
    рефакторирнг

--- Report(Bilder)/Report(Bilder)/Employee.cs
+++ Report(Bilder)/Report(Bilder)/Employee.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Report_Bilder_
+namespace Report_Builder;
 {
     public class Employee
     {
-        public string Name { get; set; }
-        public decimal Salary { get; set; }
+        public string Name { get; private set; }
+        public decimal Salary { get; private set; }
 
+        public Employee(string name, decimal salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
     }
 }
+
