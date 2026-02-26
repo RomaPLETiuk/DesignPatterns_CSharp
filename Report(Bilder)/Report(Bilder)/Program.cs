776b568ab9083baf031db624541468c08faab87f
Roman Pletiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2026-02-26 12:47:05 +0200

    Update Program.cs
    Рефакторинг

--- Report(Bilder)/Report(Bilder)/Program.cs
+++ Report(Bilder)/Report(Bilder)/Program.cs
@@ -1,20 +1,19 @@
 ﻿
-using Report_Bilder_;
+using Report_Builder;
 
-List<Employee> employees = new()
-  {
-    new Employee { Name = "Ivan", Salary = 100 },
-    new Employee { Name = "Boris", Salary = 50 },
-    new Employee { Name = "Roman", Salary = 150 },
-    new Employee { Name = "Petro", Salary = 240 },
-    new Employee { Name = "Fedor", Salary = 200 }
+var employees = new List<Employee>
+{
+    new("Ivan", 100),
+    new("Boris", 50),
+    new("Roman", 150),
+    new("Petro", 240),
+    new("Fedor", 200)
+};
 
-
-  };
-
-var builder = new EmployeeReportBilder(employees);
+IEmployeeReportBuilder builder = new EmployeeReportBuilder(employees);
 var director = new EmployeeReportDirector(builder);
-director.Build();
-var report = builder.GetReport();
+
+EmployeeReport report = director.BuildFullReport();
+
 Console.WriteLine(report);
 Console.ReadKey();
